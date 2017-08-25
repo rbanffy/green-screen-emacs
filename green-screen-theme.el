385b004c7684344901881587df98ae451dec9dd1
Ricardo Banffy <rbanffy@gmail.com>
2017-08-25 16:28:38 +0100

    Break blocks

--- green-screen-theme.el
+++ green-screen-theme.el
@@ -60,6 +60,7 @@
  '(cursor ((t (:background "#80FF00"))))
  '(escape-glyph ((t (:foreground "#264C00"))))
  '(fixed-pitch ((t (:family "Monospace"))))
+
  '(font-lock-builtin-face ((t (:foreground "#A6FF4C" :weight semi-bold))))
  '(font-lock-comment-face ((t (:foreground "#407F00" :weight normal))))
  '(font-lock-constant-face ((t (:foreground "#BFFF7F" :weight bold))))
@@ -73,30 +74,37 @@
  '(font-lock-type-face ((t (:foreground "#c0ff00" :weight bold))))
  '(font-lock-variable-name-face ((t (:foreground "lawn green" :weight normal))))
  '(font-lock-warning-face ((t (:weight bold :foreground "chartreuse"))))
+
  '(fringe ((t (:background "black" :foreground "#80FF00"))))
  '(highlight ((t (:background "green3" :foreground "lawn green" :weight normal))))
  '(hl-line ((t (:inherit highlight :background "dark green" :foreground "#80FF00"))))
  '(isearch ((((class color) (min-colors 88) (background light)) (:foreground "lightskyblue1" :background "magenta3")) (((class color) (min-colors 88) (background dark)) (:foreground "brown4" :background "palevioletred2")) (((class color) (min-colors 16)) (:foreground "cyan1" :background "magenta4")) (((class color) (min-colors 8)) (:foreground "cyan1" :background "magenta4")) (t (:inverse-video t))))
  '(isearch-fail ((((class color) (min-colors 88) (background light)) (:background "RosyBrown1")) (((class color) (min-colors 88) (background dark)) (:background "red4")) (((class color) (min-colors 16)) (:background "red")) (((class color) (min-colors 8)) (:background "red")) (((class color grayscale)) (:foreground "grey")) (t (:inverse-video t))))
  '(lazy-highlight ((((class color) (min-colors 88) (background light)) (:background "#BFFF7F")) (((class color) (min-colors 88) (background dark)) (:background "paleturquoise4")) (((class color) (min-colors 16)) (:background "#D8FFB2")) (((class color) (min-colors 8)) (:background "#D8FFB2")) (t (:underline (:color foreground-color :style line)))))
+
  '(link ((t (:foreground "#59B200" :underline t))))
  '(link-visited ((t (:inherit link))))
  '(linum ((t (:height 100 :weight normal :box nil :foreground "#4C9900" :background "#336600" :inherit default))))
+
  '(match ((((class color) (min-colors 88) (background light)) (:background "yellow1")) (((class color) (min-colors 88) (background dark)) (:background "RoyalBlue3")) (((class color) (min-colors 8) (background light)) (:foreground "black" :background "yellow")) (((class color) (min-colors 8) (background dark)) (:foreground "white" :background "blue")) (((type tty) (class mono)) (:inverse-video t)) (t (:background "gray"))))
  '(minibuffer-prompt ((t (:foreground "green3" :weight normal))))
+
  '(mode-line ((t (:background "#009922" :foreground "#002200" :box (:line-width 1 :color "#009944") :weight normal))))
  '(mode-line-buffer-id ((t (:underline (:color foreground-color :style line)))))
  '(mode-line-emphasis ((t (:weight bold))))
  '(mode-line-highlight ((t (:box nil))))
  '(mode-line-inactive ((t (:inherit mode-line :background "#264C00" :foreground "#A6FF4C" :box (:line-width -1 :color "#264C00") :weight light))))
+
  '(next-error ((t (:inherit (region)))))
  '(query-replace ((t (:inherit (isearch)))))
  '(region ((t (:background "#407F00"))))
  '(secondary-selection ((t (:background "#407F00"))))
  '(shadow ((t (:foreground "#407F00"))))
+
  '(trailing-whitespace ((t (:background "#BFFF7F" :foreground "black"))))
 
  '(markdown-code-face ((t (:foreground "#BFFF7F" :background "#407F00"))))
+
  '(elfeed-search-date-face ((t (:foreground "#59B200"))))
  '(elfeed-search-unread-title-face ((t (:foreground "#BFFF7F"))))
  '(elfeed-search-title-face ((t (:foreground "#A6FF4C"))))
