d062e9930077537e7c65f47d434759874963dd65
Ricardo Banffy <rbanffy@gmail.com>
2017-11-23 10:12:52 +0000

    Sort styles to make them easier to locate

--- green-screen-theme.el
+++ green-screen-theme.el
@@ -59,6 +59,12 @@
  '(button ((t (:inherit (link)))))
  '(cursor ((t (:background "#80FF00"))))
  '(escape-glyph ((t (:foreground "#264C00"))))
+
+ '(elfeed-search-date-face ((t (:foreground "#59B200"))))
+ '(elfeed-search-feed-face ((t (:foreground "#D8FFB2"))))
+ '(elfeed-search-title-face ((t (:foreground "#A6FF4C"))))
+ '(elfeed-search-unread-title-face ((t (:foreground "#BFFF7F"))))
+
  '(fixed-pitch ((t (:family "Monospace"))))
 
  '(font-lock-builtin-face ((t (:foreground "#A6FF4C" :weight semi-bold))))
@@ -76,17 +82,28 @@
  '(font-lock-warning-face ((t (:weight bold :foreground "chartreuse"))))
 
  '(fringe ((t (:background "black" :foreground "#80FF00"))))
+
  '(highlight ((t (:background "green3" :foreground "lawn green" :weight normal))))
+
  '(hl-line ((t (:inherit highlight :background "dark green" :foreground "#80FF00"))))
+
  '(isearch ((((class color) (min-colors 88) (background light)) (:foreground "lightskyblue1" :background "magenta3")) (((class color) (min-colors 88) (background dark)) (:foreground "brown4" :background "palevioletred2")) (((class color) (min-colors 16)) (:foreground "cyan1" :background "magenta4")) (((class color) (min-colors 8)) (:foreground "cyan1" :background "magenta4")) (t (:inverse-video t))))
+
  '(isearch-fail ((((class color) (min-colors 88) (background light)) (:background "RosyBrown1")) (((class color) (min-colors 88) (background dark)) (:background "red4")) (((class color) (min-colors 16)) (:background "red")) (((class color) (min-colors 8)) (:background "red")) (((class color grayscale)) (:foreground "grey")) (t (:inverse-video t))))
+
  '(lazy-highlight ((((class color) (min-colors 88) (background light)) (:background "#BFFF7F")) (((class color) (min-colors 88) (background dark)) (:background "paleturquoise4")) (((class color) (min-colors 16)) (:background "#D8FFB2")) (((class color) (min-colors 8)) (:background "#D8FFB2")) (t (:underline (:color foreground-color :style line)))))
 
  '(link ((t (:foreground "#59B200" :underline t))))
  '(link-visited ((t (:inherit link))))
+
  '(linum ((t (:height 100 :weight normal :box nil :foreground "#4C9900" :background "#336600" :inherit default))))
 
+ '(markdown-code-face ((t (:foreground "#BFFF7F" :background "#407F00" :family "IBM 3270 Narrow"))))
+
  '(match ((((class color) (min-colors 88) (background light)) (:background "yellow1")) (((class color) (min-colors 88) (background dark)) (:background "RoyalBlue3")) (((class color) (min-colors 8) (background light)) (:foreground "black" :background "yellow")) (((class color) (min-colors 8) (background dark)) (:foreground "white" :background "blue")) (((type tty) (class mono)) (:inverse-video t)) (t (:background "gray"))))
+
+ '(message-header-other ((t (:foreground "#D8FFB2"))))
+
  '(minibuffer-prompt ((t (:foreground "green3" :weight normal))))
 
  '(mode-line ((t (:background "#009922" :foreground "#002200" :box (:line-width 1 :color "#009944") :weight normal))))
@@ -96,23 +113,19 @@
  '(mode-line-inactive ((t (:inherit mode-line :background "#264C00" :foreground "#A6FF4C" :box (:line-width -1 :color "#264C00") :weight light))))
 
  '(next-error ((t (:inherit (region)))))
+
  '(query-replace ((t (:inherit (isearch)))))
+
  '(region ((t (:background "#407F00"))))
+
  '(secondary-selection ((t (:background "#407F00"))))
+
  '(shadow ((t (:foreground "#407F00"))))
 
  '(trailing-whitespace ((t (:background "#BFFF7F" :foreground "black"))))
 
- '(markdown-code-face ((t (:foreground "#BFFF7F" :background "#407F00" :family "IBM 3270 Narrow"))))
-
- '(elfeed-search-date-face ((t (:foreground "#59B200"))))
- '(elfeed-search-unread-title-face ((t (:foreground "#BFFF7F"))))
- '(elfeed-search-title-face ((t (:foreground "#A6FF4C"))))
- '(elfeed-search-feed-face ((t (:foreground "#D8FFB2"))))
-
- '(message-header-other ((t (:foreground "#D8FFB2"))))
-
  '(variable-pitch ((t (:family "Sans Serif"))))
+
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#80ff00" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :width normal :foundry "PfEd" :family "IBM 3270 Narrow"))))
  )
 
